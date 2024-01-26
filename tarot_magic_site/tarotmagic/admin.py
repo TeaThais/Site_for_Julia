70a5a66e1fb57da1309570d50ff35169fdeeb367
Tais <72388962+TeaThais@users.noreply.github.com>
2024-01-27 00:01:51 +0100

    images_in_admin

--- tarot_magic_site/tarotmagic/admin.py
+++ tarot_magic_site/tarotmagic/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin, messages
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 from .models import MainPage, Offer, Polytheism, Magic, Tarot, Status
@@ -9,18 +10,33 @@ admin.site.index_title = "Моя админка"
 
 @admin.register(MainPage)
 class MainPageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title','image', 'content')
+    fields = ('title', 'image', 'post_image', 'content')
+    list_display = ('id', 'title', 'post_image',  'image', 'content')
     list_display_links = ('title', )
+    readonly_fields = ('post_image', )
+
+    def post_image(self, main: MainPage):
+        if main.image:
+            return mark_safe(f"<img src='{main.image.url}' width=50>")
+        else:
+            return 'No image'
 
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title','image', 'description')
+    list_display = ('id', 'title', 'post_image', 'image', 'description')
     list_display_links = ('title',)
+    readonly_fields = ('post_image', )
+
+    def post_image(self, offer: Offer):
+        if offer.image:
+            return mark_safe(f"<img src='{offer.image.url}' width=50>")
+        else:
+            return 'No image'
 
 
 class Services(admin.ModelAdmin):
-    list_display = ('id', 'title', 'image', 'content', 'time_create', 'time_update', 'is_published')
+    list_display = ('id', 'title', 'post_image', 'image', 'content', 'time_create', 'time_update', 'is_published')
     list_display_links = ('title',)
     ordering = ('time_create', )
     list_per_page = 5
@@ -28,7 +44,7 @@ class Services(admin.ModelAdmin):
     search_fields = ('title', 'content')
     prepopulated_fields = {"slug": ("title",)}
     list_editable = ('is_published',)
-
+    readonly_fields = ('post_image',)
 
     @admin.action(description='Опубликовать выбранные записи')
     def set_published(self, request, queryset):
@@ -43,14 +59,26 @@ class Services(admin.ModelAdmin):
 
 @admin.register(Polytheism)
 class PolytheismAdmin(Services):
-    pass
+    def post_image(self, item: Polytheism):
+        if item.image:
+            return mark_safe(f"<img src='{item.image.url}' width=50>")
+        else:
+            return 'No image'
 
 
 @admin.register(Magic)
 class MagicAdmin(Services):
-    pass
+    def post_image(self, item: Magic):
+        if item.image:
+            return mark_safe(f"<img src='{item.image.url}' width=50>")
+        else:
+            return 'No image'
 
 
 @admin.register(Tarot)
 class TarotAdmin(Services):
-    pass
+    def post_image(self, item: Tarot):
+        if item.image:
+            return mark_safe(f"<img src='{item.image.url}' width=50>")
+        else:
+            return 'No image'
